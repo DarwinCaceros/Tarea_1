f6016156d6d2394934f5d0c166b05052bf88541b
Darwin Geovanni Caceros <dcaceros@gbm.net>
2022-07-21 21:29:54 -0600

    Version_1 Tarea_1

---
+++ src/Tarea_1/Tarea_1.java
@@ -0,0 +1,15 @@
+package Tarea_1;
+
+import java.util.Scanner;
+
+public class Tarea_1 {
+
+    public static void main(String[] args) {
+
+        String nombre;
+        Scanner objeto=new Scanner(System.in);
+        System.out.println("Porfavor ingrese su nombre");
+        nombre=objeto.nextLine();
+        System.out.println("Bienvenido a Progra 2 2022  " + nombre);
+    }
+}
